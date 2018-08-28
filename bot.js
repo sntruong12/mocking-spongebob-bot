276800e475297d39ec61be979b3a5a8318d4fad6
Son Truong <sntruong12@gmail.com>
2018-08-27 20:18:01 -0500

    script to convert tweet into mocking syntax

--- bot.js
+++ bot.js
@@ -6,35 +6,68 @@ const config = require('./config.js')
 const T = new Twit(config);
 
 //
-// Check user most recent tweets
+// Check user most recent tweets and convert tweet
 //
 
 let getTweetsParams = {
-  screen_name: 'powpowson',
+  screen_name: 'tanner_johnson',
   count: 1,
   exclude_replies: true,
   include_rts: false
 };
 
-T.get('statuses/user_timeline', getTweetsParams, modifyTweet);
+let originalTweet = []
+let convertedTweetArray = [];
+let convertedTweet;
 
-function modifyTweet (err, data, response) {
-  console.log(data);
-  console.log(data[0].text);
-  let tweet = data[0].text;
-  let lowerCaseTweet = tweet.toLowerCase();
-  console.log(lowerCaseTweet);
-};
+T.get('statuses/user_timeline', getTweetsParams)
+  .catch(function (err) {
+    console.log('error');
+  })
 
-//
-// Post tweet
-//
-let postTweetParams = {
-  status: 'hello world!',
-}
+  //format tweet in all lowercase
 
-T.post('statuses/update', postTweetParams, )
+  .then(function(result) {
+    let tweet = result.data[0].text;
+    originalTweet = tweet;
+    // console.log(originalTweet[0]);
+    let lowerCaseTweet = tweet.toLowerCase();
+    // console.log(`Line 29 lowercase version of tweet: ${lowerCaseTweet}`);
+    return lowerCaseTweet;
+  })
 
-function(err, data, response) {
-  console.log(data)
-}
+  //convert tweet in mocking form pushed to an array
+
+  .then(function(result) {
+    // console.log(`Line 33 result: ${result}`);
+    for (let i = 0; i < result.length; i++) {
+      if (i === 0 || i % 2 === 0) {
+        convertedTweetArray.push(result.charAt(i).toUpperCase());
+      } else {
+        convertedTweetArray.push(result.charAt(i));
+      }
+    }
+    return convertedTweetArray;
+  })
+
+  //join array to form tweet string
+
+  .then(function(result) {
+    // console.log(`Line 44 result: ${result}`)
+    convertedTweet = result.join('');
+    console.log(convertedTweet);
+    return convertedTweet;
+  })
+
+  //post tweet
+
+  .then(function(result){
+    T.post('statuses/update', {status: `Tan: "${originalTweet}"\nme: ${result}`}, function(err, data, response) {
+      if (err) {
+        console.log('no tweet went out');
+        console.log(err);
+      } else {
+        console.log('tweeted!');
+      }
+    });
+  })
