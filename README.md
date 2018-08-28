a40eee2026bdf3510d29531d529235d897d0a721
Son Truong <sntruong12@gmail.com>
2018-08-27 22:57:42 -0500

    created new module for twit scripts, edits to readme

--- README.md
+++ README.md
@@ -1,12 +1,12 @@
-This is a Twitter bot that is will monitor another Twitter user. In event of that user tweeting, the bot will translate the tweet into the mocking Spongebob meme form.
+This is a Twitter bot that is will monitor another Twitter user. In event of that user tweeting, the bot will translate the tweet into the mocking SpongeBob meme form.
 
-See this link to learn more about the Mocking Spongebob meme.
+See this link to learn more about the Mocking SpongeBob meme.
 https://knowyourmeme.com/memes/mocking-spongebob
 
 Example:
 @twitterUser1 tweets: "sigh what is the meaning of life?"
 
 The bot will then tweet out: "SiGh wHaT Is tHe mEaNiNg oF LiFe?"
-Along with the iconic mocking Spongebob image attached to the tweet.
+Along with the iconic mocking SpongeBob image attached to the tweet.
 
 One of the first obstacles that I faced was how to trigger specific code blocks after another block of code runs first. I used the built in promise API and was able to convert the tweet data that I got when initially checking the account for tweets.
---
+++ tweet.js
@@ -0,0 +1,98 @@
+const Twit = require('twit');
+const fs = require('fs');
+const config = require('./config.js')
+
+const T = new Twit(config);
+
+module.exports = function mockingSpongeBob () {
+  //
+  // Check user most recent tweets and convert tweet
+  //
+
+  let getTweetsParams = {
+    screen_name: 'tanner_johnson',
+    count: 1,
+    exclude_replies: true,
+    include_rts: false
+  };
+
+  let originalTweet;
+  let convertedTweetArray = [];
+  let convertedTweet;
+  let mockingImage = fs.readFileSync('./assets/mocking-sb.jpg', {encoding: 'base64'});
+
+  T.get('statuses/user_timeline', getTweetsParams)
+    .catch(function (err) {
+      console.log('error');
+    })
+
+    //format tweet in all lowercase
+
+    .then(function(result) {
+      let tweet = result.data[0].text;
+      originalTweet = tweet;
+      // console.log(originalTweet[0]);
+      let lowerCaseTweet = tweet.toLowerCase();
+      // console.log(`Line 29 lowercase version of tweet: ${lowerCaseTweet}`);
+      return lowerCaseTweet;
+    })
+
+    //convert tweet in mocking form pushed to an array
+
+    .then(function(result) {
+      // console.log(`Line 33 result: ${result}`);
+      for (let i = 0; i < result.length; i++) {
+        if (i === 0 || i % 2 === 0) {
+          convertedTweetArray.push(result.charAt(i).toUpperCase());
+        } else {
+          convertedTweetArray.push(result.charAt(i));
+        }
+      }
+      return convertedTweetArray;
+    })
+
+    //join array to form tweet string
+
+    .then(function(result) {
+      // console.log(`Line 44 result: ${result}`)
+      convertedTweet = result.join('');
+      console.log(convertedTweet);
+      return convertedTweet;
+    })
+
+    //upload media to twitter
+
+    .then(function(result) {
+      T.post('media/upload', {media_data: mockingImage}, function(err, data, response) {
+        // console.log('media string for image is: ', data.media_id_string);
+        let mediaId = data.media_id_string;
+        let altText = 'An image of an yellow sponge who\'s back is bent forward. He also is acting like a chicken.';
+        let metaParams = {
+          media_id: mediaId,
+          alt_text: {
+            text:altText
+          }
+        };
+        //process media on twitter
+        T.post('media/metadata/create', metaParams, function (err, data, response) {
+          if(!err) {
+
+            let tweetParams = {
+              status: `tan: ${originalTweet}\nme: ${convertedTweet}`,
+              media_ids: [mediaId]
+            }
+            //post tweet with media attached
+            T.post('statuses/update', tweetParams, function(err, data, response) {
+              if (err) {
+                console.log('no tweets going out');
+              } else {
+                console.log('mOcKiNg tImE!');
+              }
+            })
+          } else {
+            console.log('media was not created!');
+          }
+        })
+      })
+    })
+}
